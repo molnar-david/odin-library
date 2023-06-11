3bf683034865ce2304ec3bb85cdebf23d8518ba9
Molnár Dávid <d.molnarka@hotmail.com>
2023-06-11 18:50:46 +0200

    Implement add book form

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let myLibrary = [];
 let myLibraryTable = document.getElementById("library");
 
+document.getElementById("add-book-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(document.getElementById("add-book-form"));
+
+    const newBook = new Book(formData.get("title"), formData.get("author"), formData.get("is-read") ? true : false);    // convert 'on' to true and null to false
+
+    // check if book already in table
+    if (myLibrary.some((element) => element.title === newBook.title && element.author === newBook.author)) {
+        alert("Book already added!");
+    } else {
+        addBookToLibrary(newBook);
+    }
+});
+
 function Book(title, author, isRead) {
     this.title = title;
     this.author = author;
@@ -8,15 +22,17 @@ function Book(title, author, isRead) {
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+    if (book.title && book.author) {
+        myLibrary.push(book);
 
-    let newRow = myLibraryTable.insertRow();
-    let title = newRow.insertCell();
-    let author = newRow.insertCell();
-    let isRead = newRow.insertCell();
-    title.appendChild(document.createTextNode(book.title));
-    author.appendChild(document.createTextNode(book.author));
-    isRead.appendChild(document.createTextNode(book.isRead ? "Read" : "Not read"));
+        let newRow = myLibraryTable.insertRow();
+        let title = newRow.insertCell();
+        let author = newRow.insertCell();
+        let isRead = newRow.insertCell();
+        title.appendChild(document.createTextNode(book.title));
+        author.appendChild(document.createTextNode(book.author));
+        isRead.appendChild(document.createTextNode(book.isRead ? "Read" : "Not read"));
+    }
 }
 
 addBookToLibrary(new Book("title", "author", false));
