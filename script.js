207fa082f091762185d799f09fc7379e5bc0d93c
Molnár Dávid <d.molnarka@hotmail.com>
2023-06-11 17:47:47 +0200

    Add display table

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let myLibrary = [];
+let myLibraryTable = document.getElementById("library");
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, isRead) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.isRead = isRead;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+
+    let newRow = myLibraryTable.insertRow();
+    let title = newRow.insertCell();
+    let author = newRow.insertCell();
+    let isRead = newRow.insertCell();
+    title.appendChild(document.createTextNode(book.title));
+    author.appendChild(document.createTextNode(book.author));
+    isRead.appendChild(document.createTextNode(book.isRead ? "Read" : "Not read"));
 }
 
-addBookToLibrary(new Book("title", "author", 69, false));
-addBookToLibrary(new Book("The Lord of the Rings", "J. R. R. Tolkien", 1178, true));
-addBookToLibrary(new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", 223, true));
-
-console.table(myLibrary);
+addBookToLibrary(new Book("title", "author", false));
+addBookToLibrary(new Book("The Lord of the Rings", "J. R. R. Tolkien", true));
+addBookToLibrary(new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling", true));
