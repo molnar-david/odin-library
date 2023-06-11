8f43fe27034f4598abbd85505fe50fc50d3a2ac9
Molnár Dávid <d.molnarka@hotmail.com>
2023-06-11 19:18:52 +0200

    Implement 'Remove' button

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ document.getElementById("add-book-form").addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(document.getElementById("add-book-form"));
 
-    const newBook = new Book(formData.get("title"), formData.get("author"), formData.get("is-read") ? true : false);    // convert 'on' to true and null to false
+    // Convert 'on' to true and null to false
+    const newBook = new Book(formData.get("title"), formData.get("author"), formData.get("is-read") ? true : false);
 
-    // check if book already in table
+    // Check if book already in table
     if (myLibrary.some((element) => element.title === newBook.title && element.author === newBook.author)) {
         alert("Book already added!");
     } else {
@@ -21,17 +22,39 @@ function Book(title, author, isRead) {
     this.isRead = isRead;
 }
 
+function removeBookFromLibrary(index) {
+    myLibraryTable.deleteRow(+index + 1);                // Row 0 is header row in our table
+    myLibrary.splice(index, 1);
+
+    let removeBtns = document.getElementsByClassName("remove-btn");
+
+    // Reassign indices
+    let newIndex = 0;
+    for (let removeBtn of removeBtns) {
+        removeBtn.dataset.index = newIndex++;
+    }
+    console.log(removeBtns);
+}
+
 function addBookToLibrary(book) {
     if (book.title && book.author) {
-        myLibrary.push(book);
-
         let newRow = myLibraryTable.insertRow();
         let title = newRow.insertCell();
         let author = newRow.insertCell();
         let isRead = newRow.insertCell();
+
+        let removeBtn = document.createElement("button");
+        removeBtn.classList.add("remove-btn");
+        removeBtn.textContent = "Remove";
+        removeBtn.dataset.index = myLibrary.length;     // data-index
+        removeBtn.addEventListener("click", (event) => removeBookFromLibrary(event.target.dataset.index));
+
         title.appendChild(document.createTextNode(book.title));
         author.appendChild(document.createTextNode(book.author));
         isRead.appendChild(document.createTextNode(book.isRead ? "Read" : "Not read"));
+        newRow.insertCell().appendChild(removeBtn);
+
+        myLibrary.push(book);
     }
 }
 
